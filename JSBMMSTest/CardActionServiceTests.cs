cfc823147f53b4eab981652071985b2423a60303
avemumin <jacogitproject@gmail.com>
2025-05-18 11:58:51 +0200

    same unit test to verify

---
+++ JSBMMSTest/CardActionServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using JSBMMS.Models;
+using JSBMMS.Services;
+using JSBMMS.Services.Interfaces;
+using Moq;
+using NuGet.Frameworks;
+
+namespace JSBMMSTest;
+
+public class CardActionServiceTests : IDisposable
+{
+  private readonly string exampleFile = "actionRuleTakNie.json";
+
+  [Fact]
+  public async Task CheckActions_FileIsMissing_ReturnErrorMsg()
+  {
+    var expectedErrorMessage = "Błędny plik JSON lub jego brak.";
+    if (File.Exists(exampleFile))
+      File.Delete(exampleFile);
+
+    var service = new Mock<ICardActionService>();
+    var cardDetails = new CardDetails("TestCard", CardType.Debit, CardStatus.Restricted, true);
+
+    service.Setup(s => s.CheckActions(It.IsAny<CardDetails>()))
+      .ReturnsAsync(new List<string> { expectedErrorMessage });
+
+    var action = await service.Object.CheckActions(cardDetails);
+
+    Assert.Single(action);
+    Assert.Equal(expectedErrorMessage, action.First());
+  }
+
+  [Fact]
+  public async Task CheckActions_FileIsEmpty_ReturnEmtpyMessage()
+  {
+    var expectedEmptyMessage = "Plik jest pusty.";
+    File.WriteAllText(exampleFile, "");
+
+    var service = new Mock<ICardActionService>();
+    var cardDetails = new CardDetails("TestCard", CardType.Debit, CardStatus.Restricted, true);
+
+    service.Setup(s => s.CheckActions(It.IsAny<CardDetails>()))
+   .ReturnsAsync(new List<string> { expectedEmptyMessage });
+
+    var action = await service.Object.CheckActions(cardDetails);
+
+    Assert.Single(action);
+    Assert.Equal(expectedEmptyMessage, action.First());
+  }
+
+  [Fact]
+  public async Task CheckActions_Valid_ReturnAllowed()
+  {
+    var jsonContent = @"
+             [
+                 {
+                     ""ActionName"": ""ACTION1"",
+                     ""CardTypeConditions"": { ""Debit"": ""TAK"" },
+                     ""CardStatusConditions"": { ""Active"": ""TAK"" }
+                 }
+             ]";
+    File.WriteAllText(exampleFile, jsonContent);
+
+    var service = new CardActionService();
+    var cardDetails = new CardDetails("TestCard", CardType.Debit, CardStatus.Active, true);
+    var actions = await service.CheckActions(cardDetails);
+
+    Assert.Single(actions);
+    Assert.Equal("ACTION1", actions.First());
+
+  }
+
+  [Fact]
+  public async Task CheckActions_Valid_NoMatchingAction()
+  {
+    var jsonContent = @"
+             [
+                 {
+                     ""ActionName"": ""ACTION1"",
+                     ""CardTypeConditions"": { ""Prepaid"": ""TAK"" },
+                     ""CardStatusConditions"": { ""Ordered"": ""TAK"" }
+                 }
+             ]";
+    File.WriteAllText(exampleFile, jsonContent);
+    var service = new CardActionService();
+    var cardDetails = new CardDetails("TestCard", CardType.Debit, CardStatus.Active, true);
+    var action = await service.CheckActions(cardDetails);
+
+    Assert.Empty(action);
+
+  }
+
+  public void Dispose()
+  {
+    if (File.Exists(exampleFile))
+      File.Delete(exampleFile);
+  }
+}
---
+++ JSBMMSTest/CardControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using JSBMMS.Controllers;
+using JSBMMS.Models;
+using JSBMMS.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace JSBMMSTest;
+
+public class CardControllerTests
+{
+  [Fact]
+  public async Task GetActions_CardNotFound()
+  {
+    var cardService = new Mock<ICardService>();
+    var actionService = new Mock<ICardActionService>();
+
+    cardService.Setup(s => s.GetCardDetails(It.IsAny<string>(), It.IsAny<string>()))
+      .ReturnsAsync((CardDetails?)null);
+
+    var controller = new CardController(cardService.Object, actionService.Object);
+
+    var result = await controller.GetActions("User1", "CardSomeDoesNotExists");
+
+    Assert.IsType<NotFoundObjectResult>(result);
+
+  }
+
+  [Fact]
+  public async Task GetActions_ReturnOK()
+  {
+    var cardDetails = new CardDetails("TestCard", CardType.Debit, CardStatus.Active, true);
+
+    var cardService = new Mock<ICardService>();
+    var actionService = new Mock<ICardActionService>();
+
+    cardService.Setup(s => s.GetCardDetails(It.IsAny<string>(), It.IsAny<string>()))
+     .ReturnsAsync(cardDetails);
+
+    actionService.Setup(s => s.CheckActions(cardDetails))
+      .ReturnsAsync(new List<string> { "ACTION1", "ACTION2" });
+
+    var controller = new CardController(cardService.Object, actionService.Object);
+
+    var result = await controller.GetActions("User1", "TestCard");
+
+    var ok = Assert.IsType<OkObjectResult>(result);
+    Assert.NotNull(ok.Value);
+  }
+}
